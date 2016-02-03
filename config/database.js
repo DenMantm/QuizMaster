d3864b46f7cca370bd45f26c5e86b4d899702c52
kamillasecki <kamil.lasecki@eircom.net>
2016-02-03 15:57:58 +0000

    updated database link

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://user:user@ds041924.mongolab.com:41924/testbase'// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://admin:admin@ds053295.mongolab.com:53295/quizmaster'// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
