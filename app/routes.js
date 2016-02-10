6e8ae385c2ddd14e2fd6f45636d0b8a55d8c3020
kamillasecki <kamil.lasecki@eircom.net>
2016-02-10 17:45:34 +0000

    mid work for checking if the quiz name already exsist

--- app/routes.js
+++ app/routes.js
@@ -9,30 +9,26 @@
  * @reference https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens
  * @author Deniss Strods, x14100398
  *
-
  */ 
  //controller for quizes
  var user,
 	 condition = false; //this value is set accordingly if you are logged in or not
 	 
-
 var standardCtrl = require("../controllers/quiz.server.controller.js");
 //controler for users
 var UserCtrl = require("../controllers/user.server.controller.js");
 module.exports = function(app, passport) {
-
+	
 		// ==================================
 		// == ROUTES FOR QUIZ GET, POST =====
 		// ==================================
 			
 		app.get('/newquiz', function(req, res) {
-
-		res.render('newquiz.ejs', {
-			user: req.user,
-			message : {text : "none"}
-		});
-
-	});
+				res.render('newquiz.ejs', {
+					user: req.user,
+					message : {text : "none"}
+				});
+			});
 		//body with data is passed to standardCtrl
 		app.post('/newquiz', function(req, res) {
 			  standardCtrl.create(req.body);  //saving object to database
@@ -50,14 +46,11 @@ module.exports = function(app, passport) {
 		//Getting user info 
 		app.get('/updateUser',isLoggedIn, function(req, res) {
 		res.render('profile.ejs',{user : req.user, date:Date.now()});
-
 		});
-	});
 
 	// =====================================
 	// HOME PAGE (with login links) ========
 	// =====================================
-
 		app.get('/', isLoggedIn, function(req, res) {
 			
 		res.render('index.ejs', {
@@ -71,20 +64,18 @@ module.exports = function(app, passport) {
 	// show the login form
 	app.get('/login', function(req, res) {
 		// render the page and pass in any flash data if it exists
-		res.render('login.ejs', {
-			message: req.flash('loginMessage')
-		});
+		res.render('login.ejs', { message: req.flash('loginMessage') });
 	});
-	app.get('/index', isLoggedIn, function(req, res) {
+		app.get('/index', isLoggedIn, function(req, res) {
 		// render the page and pass in any flash data if it exists
 		res.render('index.ejs');
 	});
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-		successRedirect: '/index', // redirect to the secure profile section
-		failureRedirect: '/login', // redirect back to the signup page if there is an error
-		failureFlash: true // allow flash messages
+		successRedirect : '/index', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
 	}));
 	// =====================================
 	// SIGNUP ==============================
@@ -93,16 +84,14 @@ module.exports = function(app, passport) {
 	app.get('/signup', function(req, res) {
 
 		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', {
-			message: req.flash('signupMessage')
-		});
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
 	});
 
 	// process the signup form
 	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect: '/index', // redirect to the secure profile section
-		failureRedirect: '/signup', // redirect back to the signup page if there is an error
-		failureFlash: true // allow flash messages
+		successRedirect : '/index', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
 	}));
 
 	// =====================================
@@ -112,7 +101,7 @@ module.exports = function(app, passport) {
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/profile', isLoggedIn, function(req, res) {
 		res.render('profile.ejs', {
-			user: req.user // get the user out of session and pass to template
+			user : req.user // get the user out of session and pass to template
 		});
 	});
 
@@ -136,8 +125,6 @@ function isLoggedIn(req, res, next) {
 	condition = false;
 	res.redirect('/login');
 }
-<<<<<<< HEAD
-=======
 
 //Asynchrosity generation function REF: 
 //http://stackoverflow.com/questions/11278018/how-to-execute-a-javascript-function-only-after-multiple-other-functions-have-co
@@ -170,4 +157,3 @@ var when = function() {
   //  		UserCtrl.updateUser(req,res,user);
     	
 		// });
->>>>>>> 8a807d8584ecd86036af32c7b6b7b6d016fedfe2
